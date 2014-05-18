9f05eae87dbff307e1e5a9d5c1a5760a39099d18
FODE <827366980@qq.com>
2014-05-18 09:46:39 -0400

    Test git diff command!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		System.out.println("hello world! My name is sanglei!");
+		System.out.println("What is your name?");
+		System.out.println("My name is xiaofei!");
 	}
 }
